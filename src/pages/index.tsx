137ce75317b8dd5b1fbd7d6f26f6cab9a5f4cdae
Gourab Porey <gourabporey@users.noreply.github.com>
2024-01-10 23:51:24 +0530

    Add responsiveness for small screens

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import TodoList from '@/components/TodoList';
 
 const Home = () => {
   return (
-    <main className='container p-5 w-4/12 flex items-center flex-col m-auto'>
+    <main className='container p-5 sm:w-full md:4/12 xl:w-4/12 flex items-center flex-col m-auto'>
       <h2 className='text-center text-indigo-500 text-2xl'>TODO</h2>
       <div className='w-full'>
         <TodoList />
